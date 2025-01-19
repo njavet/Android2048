3b6fa5266c08eff199d35ad4c8ed019f5c4c4232
Noe Javet <np.javet@gmail.com>
2025-01-19 15:45:16 +0100

    remember problem

--- app/src/main/java/com/example/android2048/MainActivity.kt
+++ app/src/main/java/com/example/android2048/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
