6e074e5186afbccad847fe7398ba0f4bd372faf2
Noe Javet <np.javet@gmail.com>
2025-01-21 13:55:56 +0100

    change unit tests

--- app/src/test/java/com/example/android2048/MergeUnitTest.kt
+++ app/src/test/java/com/example/android2048/MergeUnitTest.kt
@@ -22,14 +22,14 @@ class MergeUnitTest {
             mutableListOf(0, 4, 0, 2)
         )
 
-        gameLogic.mergeLeft()
+        val (merged, score) = gameLogic.mergeLeft(gameLogic.board)
         assertEquals(
             listOf(
                 mutableListOf(4, 4, 0, 0),
                 mutableListOf(2, 8, 16, 0),
                 mutableListOf(4, 0, 0, 0),
                 mutableListOf(4, 2, 0, 0)
-            ), gameLogic.board
+            ), merged
         )
     }
 
@@ -42,14 +42,14 @@ class MergeUnitTest {
             mutableListOf(0, 0, 4, 0),
             mutableListOf(0, 4, 0, 2)
         )
-        gameLogic.mergeRight()
+        val (merged, score) = gameLogic.mergeRight(gameLogic.board)
         assertEquals(
             listOf(
                 mutableListOf(0, 0, 4, 4),
                 mutableListOf(0, 2, 8, 16),
                 mutableListOf(0, 0, 0, 4),
                 mutableListOf(0, 0, 4, 2)
-            ), gameLogic.board
+            ), merged
         )
     }
 
@@ -62,14 +62,14 @@ class MergeUnitTest {
             mutableListOf(0, 0, 4, 0),
             mutableListOf(0, 4, 0, 2)
         )
-        gameLogic.mergeUp()
+        val (merged, score) = gameLogic.mergeUp(gameLogic.board)
         assertEquals(
             listOf(
                 mutableListOf(4, 2, 2, 2),
                 mutableListOf(0, 8, 8, 16),
                 mutableListOf(0, 0, 0, 2),
                 mutableListOf(0, 0, 0, 0)
-            ), gameLogic.board
+            ), merged
         )
     }
 
@@ -82,14 +82,14 @@ class MergeUnitTest {
             mutableListOf(0, 0, 4, 0),
             mutableListOf(0, 4, 0, 2)
         )
-        gameLogic.mergeDown()
+        val (merged, score) = gameLogic.mergeDown(gameLogic.board)
         assertEquals(
             listOf(
                 mutableListOf(0, 0, 0, 0),
                 mutableListOf(0, 0, 0, 2),
                 mutableListOf(0, 2, 2, 16),
                 mutableListOf(4, 8, 8, 2)
-            ), gameLogic.board
+            ), merged
         )
 
     }
